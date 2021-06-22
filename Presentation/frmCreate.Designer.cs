7b85607d2dea438ad19a08f008a951810a0e9cbc
NETBOSS <paguti032020@hotmail.com>
2021-06-22 02:55:59 -0600

    xd

--- Presentation/frmCreate.Designer.cs
+++ Presentation/frmCreate.Designer.cs
@@ -29,23 +29,146 @@ namespace Presentation
         /// </summary>
         private void InitializeComponent()
         {
+            this.lblCorrectInput = new System.Windows.Forms.Label();
+            this.lblBadInputs = new System.Windows.Forms.Label();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnAcept = new System.Windows.Forms.Button();
+            this.txtNumberPhone = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.txtName = new System.Windows.Forms.TextBox();
-            this.txtLastName = new System.Windows.Forms.TextBox();
-            this.txtEmail = new System.Windows.Forms.TextBox();
-            this.txtNumberPhone = new System.Windows.Forms.TextBox();
-            this.btnAcept = new System.Windows.Forms.Button();
-            this.btnCancel = new System.Windows.Forms.Button();
-            this.lblBadInputs = new System.Windows.Forms.Label();
-            this.lblCorrectInput = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // lblCorrectInput
+            // 
+            this.lblCorrectInput.AutoSize = true;
+            this.lblCorrectInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCorrectInput.ForeColor = System.Drawing.Color.Lime;
+            this.lblCorrectInput.Location = new System.Drawing.Point(173, 369);
+            this.lblCorrectInput.Name = "lblCorrectInput";
+            this.lblCorrectInput.Size = new System.Drawing.Size(240, 31);
+            this.lblCorrectInput.TabIndex = 27;
+            this.lblCorrectInput.Text = "Usuario Agregado!";
+            // 
+            // lblBadInputs
+            // 
+            this.lblBadInputs.AutoSize = true;
+            this.lblBadInputs.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBadInputs.ForeColor = System.Drawing.Color.Red;
+            this.lblBadInputs.Location = new System.Drawing.Point(173, 369);
+            this.lblBadInputs.Name = "lblBadInputs";
+            this.lblBadInputs.Size = new System.Drawing.Size(332, 31);
+            this.lblBadInputs.TabIndex = 26;
+            this.lblBadInputs.Text = "Rellene todos los campos!";
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.FlatAppearance.BorderSize = 0;
+            this.btnCancel.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(119)))), ((int)(((byte)(182)))));
+            this.btnCancel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancel.ForeColor = System.Drawing.Color.White;
+            this.btnCancel.Location = new System.Drawing.Point(386, 443);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(91, 44);
+            this.btnCancel.TabIndex = 25;
+            this.btnCancel.Text = "Cancelar";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // btnAcept
+            // 
+            this.btnAcept.FlatAppearance.BorderSize = 0;
+            this.btnAcept.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(119)))), ((int)(((byte)(182)))));
+            this.btnAcept.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAcept.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAcept.ForeColor = System.Drawing.Color.White;
+            this.btnAcept.Location = new System.Drawing.Point(284, 443);
+            this.btnAcept.Name = "btnAcept";
+            this.btnAcept.Size = new System.Drawing.Size(91, 44);
+            this.btnAcept.TabIndex = 24;
+            this.btnAcept.Text = "Aceptar";
+            this.btnAcept.UseVisualStyleBackColor = true;
+            this.btnAcept.Click += new System.EventHandler(this.btnAcept_Click);
+            // 
+            // txtNumberPhone
+            // 
+            this.txtNumberPhone.Location = new System.Drawing.Point(231, 291);
+            this.txtNumberPhone.Name = "txtNumberPhone";
+            this.txtNumberPhone.Size = new System.Drawing.Size(209, 20);
+            this.txtNumberPhone.TabIndex = 23;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(231, 247);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(209, 20);
+            this.txtEmail.TabIndex = 22;
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.Location = new System.Drawing.Point(231, 207);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(209, 20);
+            this.txtLastName.TabIndex = 21;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(231, 170);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(209, 20);
+            this.txtName.TabIndex = 20;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(49, 291);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(169, 25);
+            this.label5.TabIndex = 19;
+            this.label5.Text = "Numero telefonico";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(49, 247);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(60, 25);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "Email";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(49, 207);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(92, 25);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "Apellidos";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(49, 170);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 25);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "Nombre";
             // 
             // panel1
             // 
@@ -54,8 +177,8 @@ namespace Presentation
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(588, 49);
-            this.panel1.TabIndex = 0;
+            this.panel1.Size = new System.Drawing.Size(604, 49);
+            this.panel1.TabIndex = 15;
             // 
             // pictureBox1
             // 
@@ -68,137 +191,12 @@ namespace Presentation
             this.pictureBox1.TabStop = false;
             this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(49, 122);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(81, 25);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Nombre";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.ForeColor = System.Drawing.Color.White;
-            this.label3.Location = new System.Drawing.Point(49, 159);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(92, 25);
-            this.label3.TabIndex = 3;
-            this.label3.Text = "Apellidos";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.ForeColor = System.Drawing.Color.White;
-            this.label4.Location = new System.Drawing.Point(49, 199);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(60, 25);
-            this.label4.TabIndex = 4;
-            this.label4.Text = "Email";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.ForeColor = System.Drawing.Color.White;
-            this.label5.Location = new System.Drawing.Point(49, 243);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(169, 25);
-            this.label5.TabIndex = 5;
-            this.label5.Text = "Numero telefonico";
-            this.label5.Click += new System.EventHandler(this.label5_Click);
-            // 
-            // txtName
-            // 
-            this.txtName.Location = new System.Drawing.Point(231, 122);
-            this.txtName.Name = "txtName";
-            this.txtName.Size = new System.Drawing.Size(209, 20);
-            this.txtName.TabIndex = 7;
-            // 
-            // txtLastName
-            // 
-            this.txtLastName.Location = new System.Drawing.Point(231, 159);
-            this.txtLastName.Name = "txtLastName";
-            this.txtLastName.Size = new System.Drawing.Size(209, 20);
-            this.txtLastName.TabIndex = 8;
-            // 
-            // txtEmail
-            // 
-            this.txtEmail.Location = new System.Drawing.Point(231, 199);
-            this.txtEmail.Name = "txtEmail";
-            this.txtEmail.Size = new System.Drawing.Size(209, 20);
-            this.txtEmail.TabIndex = 9;
-            // 
-            // txtNumberPhone
-            // 
-            this.txtNumberPhone.Location = new System.Drawing.Point(231, 243);
-            this.txtNumberPhone.Name = "txtNumberPhone";
-            this.txtNumberPhone.Size = new System.Drawing.Size(209, 20);
-            this.txtNumberPhone.TabIndex = 10;
-            // 
-            // btnAcept
-            // 
-            this.btnAcept.FlatAppearance.BorderSize = 0;
-            this.btnAcept.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(119)))), ((int)(((byte)(182)))));
-            this.btnAcept.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnAcept.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnAcept.ForeColor = System.Drawing.Color.White;
-            this.btnAcept.Location = new System.Drawing.Point(284, 395);
-            this.btnAcept.Name = "btnAcept";
-            this.btnAcept.Size = new System.Drawing.Size(91, 44);
-            this.btnAcept.TabIndex = 11;
-            this.btnAcept.Text = "Aceptar";
-            this.btnAcept.UseVisualStyleBackColor = true;
-            this.btnAcept.Click += new System.EventHandler(this.btnAcept_Click);
-            // 
-            // btnCancel
-            // 
-            this.btnCancel.FlatAppearance.BorderSize = 0;
-            this.btnCancel.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(119)))), ((int)(((byte)(182)))));
-            this.btnCancel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnCancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnCancel.ForeColor = System.Drawing.Color.White;
-            this.btnCancel.Location = new System.Drawing.Point(386, 395);
-            this.btnCancel.Name = "btnCancel";
-            this.btnCancel.Size = new System.Drawing.Size(91, 44);
-            this.btnCancel.TabIndex = 12;
-            this.btnCancel.Text = "Cancelar";
-            this.btnCancel.UseVisualStyleBackColor = true;
-            // 
-            // lblBadInputs
-            // 
-            this.lblBadInputs.AutoSize = true;
-            this.lblBadInputs.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblBadInputs.ForeColor = System.Drawing.Color.Red;
-            this.lblBadInputs.Location = new System.Drawing.Point(173, 321);
-            this.lblBadInputs.Name = "lblBadInputs";
-            this.lblBadInputs.Size = new System.Drawing.Size(332, 31);
-            this.lblBadInputs.TabIndex = 13;
-            this.lblBadInputs.Text = "Rellene todos los campos!";
-            this.lblBadInputs.Click += new System.EventHandler(this.lblBadInputs_Click);
-            // 
-            // lblCorrectInput
-            // 
-            this.lblCorrectInput.AutoSize = true;
-            this.lblCorrectInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblCorrectInput.ForeColor = System.Drawing.Color.Lime;
-            this.lblCorrectInput.Location = new System.Drawing.Point(173, 321);
-            this.lblCorrectInput.Name = "lblCorrectInput";
-            this.lblCorrectInput.Size = new System.Drawing.Size(240, 31);
-            this.lblCorrectInput.TabIndex = 14;
-            this.lblCorrectInput.Text = "Usuario Agregado!";
-            // 
             // frmCreate
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(75)))), ((int)(((byte)(99)))));
-            this.ClientSize = new System.Drawing.Size(588, 495);
+            this.ClientSize = new System.Drawing.Size(604, 534);
             this.Controls.Add(this.lblCorrectInput);
             this.Controls.Add(this.lblBadInputs);
             this.Controls.Add(this.btnCancel);
@@ -224,19 +222,19 @@ namespace Presentation
 
         #endregion
 
+        private System.Windows.Forms.Label lblCorrectInput;
+        private System.Windows.Forms.Label lblBadInputs;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnAcept;
+        private System.Windows.Forms.TextBox txtNumberPhone;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.TextBox txtName;
-        private System.Windows.Forms.TextBox txtLastName;
-        private System.Windows.Forms.TextBox txtEmail;
-        private System.Windows.Forms.TextBox txtNumberPhone;
-        private System.Windows.Forms.Button btnAcept;
-        private System.Windows.Forms.Button btnCancel;
-        private System.Windows.Forms.Label lblBadInputs;
-        private System.Windows.Forms.Label lblCorrectInput;
     }
 }
