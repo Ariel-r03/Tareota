ad961ed07112efed07bcb563db4a08c0abfc5402
Ariel <arieladolfo.silva@gmail.com>
2021-06-22 13:08:05 -0600

    Al fin

--- Presentation/ProductDetails.cs
+++ Presentation/ProductDetails.cs
@@ -7,14 +7,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Core.Poco;
 
 namespace Presentation
 {
     public partial class ProductDetails : UserControl
     {
+
+        public Product product { get; set; }
         public ProductDetails()
         {
             InitializeComponent();
+            product = new Product();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void ProductDetails_Load(object sender, EventArgs e)
+        {
+            pcb.Image = Image.FromFile(product.ImageURL);
+
+
+            txtName.Text = product.Name;
+            txtBrand.Text = product.Brand;
+            txtModel.Text = product.Model;
+            txtPrice.Text = Convert.ToString(product.Price);
+            txtDesc.Text = product.Description;
         }
     }
 }
